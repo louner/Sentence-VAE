b599ce6c641c123a6393a2532e3745718a66e515
allen_kuo <allen_kuo@trendmicro.com>
2019-07-26 01:51:02 +0000

    add read_txt

--- encode.py
+++ encode.py
@@ -8,6 +8,13 @@ from sklearn.externals import joblib
 
 import sys
 
+def read_txt(txt_file):
+    with open(txt_file) as f:
+        lines = f.read().split('\n')
+    df = pd.DataFrame()
+    df['url'] = lines
+    return df
+
 if __name__ == '__main__':
     model_fpath, test_file = sys.argv[1:]
 
@@ -15,7 +22,8 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         model.cuda()
 
-    test = pd.read_csv(test_file, names=['url'], engine='python', error_bad_lines=False, warn_bad_lines=False)
+    #test = pd.read_csv(test_file, names=['url'], engine='python', error_bad_lines=False, warn_bad_lines=False)
+    test = read_txt(test_file)
     model.ptb.create_data(test)
 
     data_loader = DataLoader(dataset=model.ptb, batch_size=1024, shuffle=False, num_workers=cpu_count())
@@ -24,8 +32,9 @@ if __name__ == '__main__':
     for iteration, batch in enumerate(data_loader):
         batch_to_var(batch)
         logp, mean, logv, z, encoder_last = model(batch['input'], batch['length'])
+        z = encoder_last
         #logp, mean, logv, z = model(to_var(batch['input']), to_var(batch['length']))
         encoded.append(z.tolist())
         print(iteration, z.size())
     encoded = np.vstack(encoded)
-    np.savetxt('%s.encoded.npy'%(test_file), encoded)
+    joblib.dump(encoded, '%s.encoder_last'%(test_file))
